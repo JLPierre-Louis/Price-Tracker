66057a4629213776f5de916c800bbac42528e80a
JLPierre-Louis <luco_pierre@yhahoo.com>
2018-07-17 20:43:50 -0400

    Added Item Class

--- src/main.py
+++ src/main.py
@@ -6,16 +6,40 @@ Python Version: 3.7
 
 import argparse
 
+
 def init_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url",
                         dest="url",
                         help="The url to be searched")
+    parser.add_argument("-v", "--verbose",
+                        dest="verbose",
+                        action='store_true',
+                        default=False,
+                        help="Determine if the user wants it to be verbose")
+    parser.add_argument("-s", "--store",
+                        dest="store_directory",
+                        help="The relative path to directory where to store")
+    return parser
+
 
 def main():
     pass
 
 
-
 if __name__ == '__main__':
     main()
+
+
+class Item(object):
+    def __init__(self, price, url, email, name="", brand="", priority=0,
+                 starred=False, group=None):
+        self._name = name
+        self._price = price
+        self._last_accessed = None   #future
+        self._priority = priority
+        self._starred = starred
+        self._group = group
+        self._url = url
+        self._email = email
+        self._brand = brand
