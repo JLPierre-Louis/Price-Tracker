cba3c2b8e7defbee64bb498d2a68e507fcd244d6
JLPierre-Louis <luco_pierre@yhahoo.com>
2018-07-17 20:02:28 -0400

    Starting work on argument parser
    Argument parser fields to work on command line interface tool

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""
+Author: Luco
+Author: Ryan
+Python Version: 3.7
+"""
+
+import argparse
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url",
+                        dest="url",
+                        help="The url to be searched")
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
