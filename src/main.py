6677a41cb5dd02c67a4bd232f920500836786656
Ryan Cervantes <ryan.zachary.cervantes@gmail.com>
2018-07-24 19:01:56 -0400

    added stuff to set up program

--- src/main.py
+++ src/main.py
@@ -6,28 +6,11 @@ Python Version: 3.7
 
 import argparse
 
-<<<<<<< HEAD
 def init_parser():
-=======
-
-def init_parser():
-
->>>>>>> 9146c8e2ea024e67a8f1035842979eefc8290a7f
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url",
                         dest="url",
                         help="The url to be searched")
-<<<<<<< HEAD
-
-def main(args):
-    pass    
-
-
-if __name__ == '__main__':
-    parser = init_parser()
-    args = parser.parser_args()
-    main(args)
-=======
     parser.add_argument("-v", "--verbose",
                         dest="verbose",
                         action='store_true',
@@ -44,7 +27,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = init_parser()
+    args = parser.parse_args()
+    main(args)
 
 
->>>>>>> 9146c8e2ea024e67a8f1035842979eefc8290a7f
